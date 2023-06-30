e233979c5be11b3d61d17283170a7671f7dc1e98
sbulieienko <sergey.bulyeyenko@gmail.com>
2023-06-30 17:23:37 +0200

    task 2.3.G

---
+++ handbook/python/task-2.3.G.py
@@ -0,0 +1,22 @@
+a = int(input())
+b = int(input())
+
+if a >= b:
+    big = a
+    small = b
+else:
+    big = b
+    small = a
+
+r = None
+
+while r != 0:
+    r = big % small
+    big = small
+    small = r
+
+nod = big
+
+nok = a * b / nod
+
+print(int(nok))
